eaece32fc42d232279ea9d8c5bd9c526ed8d9106
Hugo Huart <hugo@huart.ch>
2025-07-30 16:59:50 +0200

    emacs: update fontsize, C indentation and multiple cursors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,11 +9,13 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (load-theme 'gruber-darker t)
-(set-frame-font "Comic Mono 16" nil t)
+(set-frame-font "Comic Mono 18" nil t)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (setq inhibit-startup-screen t)
+(setq c-basic-offset 4)
 
 (add-to-list 'backup-directory-alist (cons "." "~/.emacs.d/backups/"))
 
 (global-set-key [f5] 'recompile)
+(global-set-key (kbd "C-S-c") 'mc/edit-lines)
